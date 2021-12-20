1910794de8a8cd8e09279428e5a754817a3e25f6
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-12-20 17:10:01 +0200

    cookie-stand-admin

---
+++ component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className="flex justify-between bg-emerald-700 py-4 items-center">
+                <a href="career" className='m-1'>Careers</a>
+            </footer>
+        </div>
+    )
+}
